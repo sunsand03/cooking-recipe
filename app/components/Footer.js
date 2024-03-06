86e2c7122f9e121a6260e16706766371afae08e9
Sandrine IBRAIMA-NOMBO <sandrinenombo03@gmail.com>
2024-03-06 10:26:19 +0100

    ajout du footer

---
+++ app/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from '../styles/footer.module.css'
+
+const Footer = () => {
+    return (
+        <div className ={style.container}>
+            <p>&copy; Site créé par Sandrine NOMBO | 2024</p>
+        </div>
+    );
+};
+
+export default Footer;
